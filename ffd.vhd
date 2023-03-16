e4cb51e95c4b230c7ff01c2feab6e973733edaf2
aclagoria <lagoriacarolina@gmail.com>
2023-03-16 19:15:38 -0300

    asignacion de paquete y eliminacion de hab

--- ffd.vhd
+++ ffd.vhd
@@ -1,13 +1,24 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
+package ffd_pkg is
+    component ffd is
+        generic(
+            constant N : natural := 1);
+        port(
+            rst : in std_logic;
+            D   : in std_logic_vector (N-1 downto 0);
+            clk : in std_logic;
+            Q   : out std_logic_vector (N-1 downto 0));
+    end component;
+end package;
+
 entity ffd is
     generic(
         constant N : natural := 1);
     port(
         rst : in std_logic;
         D   : in std_logic_vector (N-1 downto 0);
-        hab : in std_logic;
         clk : in std_logic;
         Q   : out std_logic_vector (N-1 downto 0));
 end ffd;
