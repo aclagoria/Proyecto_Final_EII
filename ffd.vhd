80c67c0a177755e10522b0e525e3cad36ebb9041
aclagoria <lagoriacarolina@gmail.com>
2023-03-16 19:43:48 -0300

    correcciones en ffd

--- ffd.vhd
+++ ffd.vhd
@@ -13,6 +13,9 @@ package ffd_pkg is
     end component;
 end package;
 
+library IEEE;
+use IEEE.std_logic_1164.all;
+
 entity ffd is
     generic(
         constant N : natural := 1);
@@ -27,7 +30,7 @@ end ffd;
 architecture solucion of ffd is
 begin
     -- completar
-    FlipFlopD : process(rst,clk,hab)
+    FlipFlopD : process(rst,clk)
     begin
         if ( rst='1') then
             Q <= ( others => '0' );
